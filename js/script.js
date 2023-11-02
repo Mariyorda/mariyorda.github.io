b7deebe17c9e58f6c78415010e78c9695921667c
Mariya Izerakova <mariyordab@gmail.com>
2023-11-02 20:02:54 +0300

    refactor: minor code refactoring

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@
       footer.textContent = `Page loaded in ${(Date.now() - startTimestamp) / 1000}s`;
   
       Array.from(document.getElementsByClassName("header__navigation-link")).forEach(link => {
-        console.log(link, link.href, location.href);
         if (link.href === window.location.href) {
           link.classList.add("header__navigation-link_is-active");
         }
